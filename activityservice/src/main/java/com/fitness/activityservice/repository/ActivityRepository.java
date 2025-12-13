ea88c907438f848f5812f96af78c499b59f524bd
ekobara <emmanuelkobara@gmail.com>
2025-12-12 19:52:58 -0500

    DTO

---
+++ activityservice/src/main/java/com/fitness/activityservice/repository/ActivityRepository.java
@@ -0,0 +1,7 @@
+package com.fitness.activityservice.repository;
+
+
+import com.fitness.activityservice.model.Activity;
+
+public interface ActivityRepository extends JpaRepository<Activity, String> {
+}
---
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -0,0 +1,34 @@
+package com.fitness.activityservice.service;
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
+import com.fitness.activityservice.repository.ActivityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+
+    private ActivityResponse trackActivity(ActivityRequest request){
+        Activity activity = Activity.builder()
+                .userId(request.getUserId())
+                .type(request.getType())
+                .duration(request.getDuration())
+                .caloriesBurned(request.getCaloriesBurned())
+                .startTime(request.getStartTime())
+                .additionalMetrics(request.getAdditionalMetrics())
+                .build();
+
+        Activity savedRequest = activityRepository.save(activity);
+
+
+    }
+
+    private ActivityResponse maptoActivity(Activity activity){
+
+    }
+}
